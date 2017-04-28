373592d5b80c9009ebbdb7ee94ef03730f84a18b
HarryStevens <harryjosephstevens@gmail.com>
2017-04-28 23:30:31 +0530

    Set interval

--- index.js
+++ index.js
@@ -8,27 +8,28 @@ var lengths = {
 	prepositions: json.prepositions.length
 }
 
-
-var noun_a = get_random("nouns");
-var verb = get_random("verbs");
-var adverb = get_random("adverbs");
-var preposition = get_random("prepositions");
-var noun_b = get_random("nouns");
-
-var indefinite_article;
-if (noun_b[0].toLowerCase() == "a" || noun_b[0].toLowerCase() == "e" || noun_b[0].toLowerCase() == "i" || noun_b[0].toLowerCase() == "o" || noun_b[0].toLowerCase() == "u") {
-	indefinite_article = "an";
-} else {
-	indefinite_article = "a";
-}
-
 var possessive_pronouns = ["My", "Your", "His", "Her", "Its", "Our", "Your", "Their"];
 
-var article = [indefinite_article, "the"][rand_between(0, 1)];
+setInterval(function(){
+	make_sentence(get_random("nouns"));	
+}, 4000);
 
-var sentence = [possessive_pronouns[rand_between(0, possessive_pronouns.length - 1)], noun_a, verb, adverb, preposition, article, noun_b].join(" ") + ".";
+function make_sentence(last_word){
+	var indefinite_article;
+	if (last_word[0].toLowerCase() == "a" || last_word[0].toLowerCase() == "e" || last_word[0].toLowerCase() == "i" || last_word[0].toLowerCase() == "o" || last_word[0].toLowerCase() == "u") {
+		indefinite_article = "an";
+	} else {
+		indefinite_article = "a";
+	}
+	var article = [indefinite_article, "the"][rand_between(0, 2)];
 
-console.log(sentence);
+	console.log([possessive_pronouns[rand_between(0, possessive_pronouns.length - 1)], get_random("nouns"), get_random("verbs"), get_random("adverbs"), get_random("prepositions"), article, last_word].join(" ") + ".");
+}
+
+
+
+
+
 
 function rand_between(start, end){
 	return Math.floor(Math.random() * end) + start;
@@ -36,6 +37,6 @@ function rand_between(start, end){
 
 function get_random(pos){
 
-	var a = json[pos][rand_between(0, lengths[pos] - 1)];
+	var a = json[pos][rand_between(0, lengths[pos])];
 	return pos == "verbs" ? a.past : a;
 }
